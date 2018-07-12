fdfbe3264648de8d70e3943342f1b36dc7df44fd
Joris Veens <joris.h.j@gmail.com>
2018-07-12 12:16:41 +0200

    initial version hc

---
+++ tests/test_hc.py
@@ -0,0 +1,57 @@
+from unittest import TestCase
+import numpy as np
+
+from mclust.hc import *
+
+
+class TestHc(TestCase):
+    test_data_2d = np.array([[(i * j + 4 * (i - j * (.5 * i - 8)) % 12) for i in range(3)] for j in range(8)], float, order='F')
+    test_data_sclaed = np.array([[- 1.080123,  - 1.4639385, - 0.5400617],
+                                [1.080123, 0.1126107, - 1.6201852],
+                                [0.000000, - 1.0134959, - 0.5400617],
+                                [- 1.080123, 0.5630533, 0.5400617],
+                                [1.080123, - 0.5630533, - 0.5400617],
+                                [0.000000, 1.0134959, 0.5400617],
+                                [- 1.080123, - 0.1126107, 1.6201852],
+                                [1.080123, 1.4639385, 0.5400617]])
+
+    def test_scale(self):
+        scaled = scale(self.test_data_2d, center=True, rescale=True)
+        self.assertTrue(np.allclose(self.test_data_sclaed, scaled))
+
+    def test_partconv_consec(self):
+        test_data = np.array([1, 4, 5, 4, 4, 7, 1, 2, 0])
+        expected = np.array([0, 1, 2, 1, 1, 3, 0, 4, 5]) + 1
+        result = partconv(test_data, consec=True)
+        self.assertTrue(np.array_equal(result, expected))
+
+    def test_partconv_non_consec(self):
+        test_data = np.array([1, 4, 5, 4, 4, 7, 1, 2, 0])
+        expected = np.array([0, 1, 2, 1, 1, 5, 0, 7, 8]) + 1
+        result = partconv(test_data, consec=False)
+        self.assertTrue(np.array_equal(result, expected))
+
+    def test_traceW(self):
+        result = traceW(self.test_data_2d)
+        self.assertEqual(traceW(self.test_data_2d), 450)
+
+    def test_hc(self):
+        result = hc(self.test_data_2d, "ds")
+        print(result)
+
+    def test_hclass(self):
+        z = hc(self.test_data_2d, "vvv")
+        hcpairs = hcVVV(z)
+        print(hclass(hcpairs, range(1, 9)))
+
+    def test_hcvvv(self):
+        z = hc(self.test_data_2d, "vvv")
+        result = hcVVV(z)
+        print("hcvvv")
+        print(result)
+
+    def test_hceii(self):
+        z = hc(self.test_data_2d, "eii")
+        result = hcEII(z)
+        print("hceii")
+        print(result)
