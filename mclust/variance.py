7f6555e11716fc813182a174d94722dbc8b3e1eb
Joris Veens <joris.h.j@gmail.com>
2018-08-01 16:55:33 +0200

    variance refactor

--- mclust/variance.py
+++ mclust/variance.py
@@ -11,6 +11,9 @@ class Variance:
     def get_covariance(self):
         pass
 
+    def select_group(self, index):
+        self.G = 1
+
     def __str__(self):
         return f"covariance list:\n {self.get_covariance()}"
 
@@ -18,21 +21,22 @@ class Variance:
 class VarianceSigmasq(Variance):
     def __init__(self, d, g, sigmasq):
         super().__init__(d, g)
-        self.sigmasq = np.array(sigmasq)
+        if sigmasq.ndim == 0:
+            self.sigmasq = np.repeat(sigmasq, g)
+        else:
+            self.sigmasq = np.array(sigmasq)
 
     def get_covariance(self):
-        if self.sigmasq.ndim == 0:
-            # Model E and EII
-            return np.tile(self.sigmasq * np.identity(self.d), [self.G, 1]).reshape(self.G, self.d, self.d)
-        else:
-            # Model V en VII
-            covariance = np.zeros((self.G, self.d, self.d))
-            for i in range(len(self.sigmasq)):
-                covariance[i] = np.identity(self.d) * self.sigmasq[i]
-            return covariance
+        covariance = np.zeros((self.G, self.d, self.d))
+        for i, sq in enumerate(self.sigmasq):
+            covariance[i] = np.identity(self.d) * sq
+        return covariance
+
+    def select_group(self, index):
+        super().select_group(index)
+        self.sigmasq = np.repeat(self.sigmasq[index], self.G)
 
 
-# TODO make sure covariance returs a list of dimension g, d, d
 class VarianceDecomposition(Variance):
     def __init__(self, d, g, scale, shape, orientation=None):
         super().__init__(d, g)
@@ -42,11 +46,10 @@ class VarianceDecomposition(Variance):
         self.shape = np.array(shape)
         if self.shape.ndim == 1:
             self.shape = np.tile(shape, (self.G, 1))
-        # TODO variant orientation
         if orientation is None:
-            self.orientation = np.tile(np.identity(self.d), (self.G, 1)).reshape(self.G, self.d, self.d)
+            self.orientation = np.tile(np.identity(self.d), (self.G, 1, 1))
         elif orientation.ndim == 2:
-            self.orientation = np.tile(orientation, (self.G, 1)).reshape(self.G, self.d, self.d)
+            self.orientation = np.tile(orientation, (self.G, 1, 1))
         else:
             self.orientation = orientation
 
@@ -60,21 +63,29 @@ class VarianceDecomposition(Variance):
         # return self.scale * self.orientation.dot(self.shape).dot(self.orientation.transpose())
         return covariance
 
+    def select_group(self, index):
+        super().select_group(index)
+        self.scale = np.repeat(self.scale[index], self.G)
+        self.shape = np.tile(self.shape[index], (self.G, 1))
+        self.orientation = np.tile(self.orientation[index], (self.G, 1, 1))
+
 
 class VarianceCholesky(Variance):
     def __init__(self, d, g, cholsigma):
         super().__init__(d, g)
         if cholsigma.ndim != 3:
             raise DimensionError("cholsigma should have 3 dimensions")
-        self.cholsigma = cholsigma
+        if cholsigma.shape[0] == 1:
+            self.cholsigma = np.tile(cholsigma[0], (self.G, 1, 1))
+        else:
+            self.cholsigma = cholsigma
 
     def get_covariance(self):
-        # TODO  implement unchol
         result = np.zeros((self.G, self.d, self.d))
-        if self.cholsigma.shape[0] == 1:
-            for g in range(self.G):
-                result[g] = self.cholsigma[0].transpose().dot(self.cholsigma[0])
-        else:
-            for g in range(self.G):
-                result[g] = self.cholsigma[g].transpose().dot(self.cholsigma[g])
+        for g in range(self.G):
+            result[g] = self.cholsigma[g].transpose().dot(self.cholsigma[g])
         return result
+
+    def select_group(self, index):
+        super().select_group(index)
+        self.cholsigma = np.tile(self.cholsigma[index], (self.G, 1, 1))
