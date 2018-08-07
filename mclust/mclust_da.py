b6aa97b845d28857fca313ef1e20212022b5a41d
Joris Veens <joris.h.j@gmail.com>
2018-08-07 15:49:52 +0200

    MclustDA and EDDA

--- mclust/mclust_da.py
+++ mclust/mclust_da.py
@@ -1,52 +1,145 @@
 import numpy as np
+import warnings
+import copy
+from math import log
 
-from mclust.exceptions import ModelError
+from mclust.exceptions import ModelError, AbstractMethodError
 from mclust.control import EMControl, ModelTypes
+from mclust.utility import mclust_unmap
+from mclust.model_factory import ModelFactory
+from mclust.mclust_bic import MclustBIC
 
 
-class MclustDA:
-    def __init__(self, data, classes, g=None, models=None, model_type=None, control=EMControl()):
+class DiscriminantAnalysis:
+    def __init__(self, data, classes, g=None, models=None, control=EMControl()):
         if classes is None:
             raise ValueError("class labels (classes) for training data must be provided")
-        self._handle_input(data, classes, g, models)
-
-        # continue implement MclustDA
-
-    def _handle_input(self, data, classes, g, models):
         self.data = data
-        self.n, self.p = data.shape
+        self.n = data.shape[0]
+        if data.ndim == 1:
+            self.d = 1
+        else:
+            self.d = data.shape[1]
         self.labels = list(set(classes))
-
-        if len(self.labels) == 1:
-            self.g = 1
-
-        # what is the use of a list here
+        self.nclasses = len(self.labels)
+        self.fitted_models = {}
+        self.observations = {}
         self.g = {}
+
         if g is None:
-            for i in range(len(self.labels)):
+            for i in range(self.nclasses):
                 self.g[i] = np.arange(1, 6)
         elif isinstance(g, dict):
             for key in g.keys():
                 self.g[key] = np.sort(g[key])
         else:
-            for i in range(len(self.labels)):
+            for i in range(self.nclasses):
                 self.g[i] = np.sort(g)
 
         if np.any([np.any(vals <= 0) for vals in self.g.values()]):
             raise ModelError("all values of G must be positive")
 
         if models is None:
-            if self.p == 1:
+            if self.d == 1:
                 models = ModelTypes.get_one_dimensional()
             else:
                 models = ModelTypes.get_multi_dimensional()
-        if self.n <= self.p:
+        if self.n <= self.d:
             models = list(set(ModelTypes.get_multi_dimensional()).intersection(ModelTypes.get_less_observations()))
 
+        self.models = models
         if not isinstance(models, dict):
             self.models = {}
             for i in range(len(self.labels)):
                 self.models[i] = models
 
+        self.n = {}
+
+    def predict(self, newdata=None, prior=None):
+        """For now corresponds with summary.MclustDA"""
+        group_sizes = np.array([model.n for model in self.fitted_models.values()])
+        if newdata is None:
+            newdata = self.data
+
+        if prior is None:
+            prior = group_sizes / np.sum(group_sizes)
+        else:
+            if len(prior) != self.nclasses:
+                raise ModelError("wrong number of prior probabilities")
+            if np.any(prior < 0):
+                raise ModelError("prior must be nonnegative")
+
+        z = np.zeros((newdata.shape[0], self.nclasses))
+        for key in range(self.nclasses):
+            z[:, key] = self.fitted_models[key].density(newdata, logarithm=True)
+        z = z + np.log(prior/np.sum(prior))
+        z = (z.transpose() - np.log(np.sum(np.exp(z), 1))).transpose()
+        z = np.exp(z)
+        cl = np.argmax(z, 1)
+
+        return cl
+
+    def df(self):
+        raise AbstractMethodError()
+
+    def loglik(self, new_data=None):
+        if new_data is None:
+            new_data = self.data
+        n = np.sum(list(self.n.values()))
+        logfclass = [np.log(self.n[key]/n) for key in sorted(self.n.keys())]
+        ll = np.array([mod.density(new_data, True) for mod in self.fitted_models.values()])
+        return np.sum(np.log(np.sum(np.exp(ll.transpose() + logfclass), 1)))
 
 
+
+class EDDA(DiscriminantAnalysis):
+    def __init__(self, data, classes, g=None, models=None, control=EMControl()):
+        super().__init__(data, classes, g, models, control)
+
+        z = mclust_unmap(classes)
+
+        best_model = None
+        for model in self.models[0]:
+            mod = ModelFactory.create(self.data, model, z.copy(order='F'))
+            mod.m_step()
+            mod.e_step()
+            bic = mod.bic()
+            if not np.isnan(bic) and (best_model is None or bic >= best_model.bic()):
+                best_model = mod
+        if best_model is None:
+            warnings.warn("No model(s) can be estimated!")
+            return
+
+        for l in range(self.nclasses):
+            ind = classes == self.labels[l]
+            self.fitted_models[l] = copy.deepcopy(best_model)
+            self.fitted_models[l].n = np.sum(ind)
+            self.fitted_models[l].G = 1
+            self.fitted_models[l].pro = np.array([1])
+            self.fitted_models[l].mean = np.array([self.fitted_models[l].mean[l]])
+            self.fitted_models[l].variance.select_group(l)
+            self.fitted_models[l].z = None
+
+            self.observations[l] = np.where(ind)
+            self.n[l] = np.sum(ind)
+            self.g[l] = 1
+
+    def df(self):
+        return int(self.d * self.nclasses + self.fitted_models[0].model.n_var_params(self.d, self.nclasses))
+
+
+class MclustDA(DiscriminantAnalysis):
+    def __init__(self, data, classes, g=None, models=None, control=EMControl()):
+            super().__init__(data, classes, g, models, control)
+            for l in range(self.nclasses):
+                ind = classes == self.labels[l]
+                bic = MclustBIC(self.data[ind], self.g[l], self.models[l])
+                self.fitted_models[l] = bic.pick_best_model()
+
+                self.observations[l] = np.where(ind)
+                self.g[l] = self.fitted_models[l].G
+                self.n[l] = np.sum(ind)
+
+    def df(self):
+        return int(np.sum([(mod.G -1) + mod.G * self.d + mod.model.n_var_params(self.d, mod.G)
+                       for mod in self.fitted_models.values()]))
--- tests/test_mculst_da.py
+++ tests/test_mculst_da.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 import numpy as np
 
-from mclust.mclust_da import MclustDA
+from mclust.mclust_da import MclustDA, EDDA, DiscriminantAnalysis
 from mclust.exceptions import ModelError
 
 from .utility import apply_resource
@@ -12,18 +12,18 @@ class TestMclust(TestCase):
         pass
 
     def test_handle_input_correct(self):
-        data = np.zeros((145, 3))
-        model = MclustDA(data, np.array([1, 2, 4]))
-        self.assertEqual(len(model.g), 3)
+        data = np.array([12, 13, 56, 60])
+        model = DiscriminantAnalysis(data, np.array([1, 1, 2, 2]))
+        self.assertEqual(len(model.g), 2)
         self.assertTrue(np.array_equal(model.g[0], np.arange(1, 6)))
 
-        model = MclustDA(data, np.array([1, 3, 4]), g=[1, 3, 4, 5])
+        model = DiscriminantAnalysis(data, np.array([1, 3, 4]), g=[1, 3, 4, 5])
         self.assertTrue(len(model.g), 3)
         self.assertTrue(np.array_equal(model.g[0], [1, 3, 4, 5]))
 
         g = {0: np.array([2, 3, 4, 1], dtype=int),
              1: np.array([1, 2, 3], dtype=int)}
-        model = MclustDA(data, np.array([1, 3]), g)
+        model = DiscriminantAnalysis(data, np.array([1, 3]), g)
         self.assertEqual(len(model.g), 2)
         self.assertTrue(np.array_equal(model.g[0], np.array([1, 2, 3, 4])))
         self.assertTrue(np.array_equal(model.g[1], np.array([1, 2, 3])))
@@ -31,6 +31,47 @@ class TestMclust(TestCase):
     def test_handle_input_failure(self):
         data = np.zeros((145, 3))
         with self.assertRaises(ModelError):
-            MclustDA(data, np.array([1, 2, 3]), g=np.array([0, 1, 2, 3]))
+            DiscriminantAnalysis(data, np.array([1, 2, 3]), g=np.array([0, 1, 2, 3]))
+
+    def test_edda(self):
+        data = apply_resource('data_sets', "diabetes.csv",
+                              lambda f: np.genfromtxt(f, skip_header=1, delimiter=','))
+        classes = apply_resource('data_sets', "diabetes_classification.csv",
+                                 lambda f: np.genfromtxt(f, delimiter=','))
+
+        model = EDDA(data, classes)
+
+        print(model.predict())
+        print(list(model.n))
+
+    def test_mclust_da(self):
+        data = apply_resource('data_sets', "diabetes.csv",
+                              lambda f: np.genfromtxt(f, skip_header=1, delimiter=','))
+        classes = apply_resource('data_sets', "diabetes_classification.csv",
+                                 lambda f: np.genfromtxt(f, delimiter=','))
+
+        model = MclustDA(data, classes)
+        for mod in model.fitted_models.values():
+            print(mod)
+
+        print(model.predict())
+
+    def test_loglik(self):
+        data = apply_resource('data_sets', "diabetes.csv",
+                              lambda f: np.genfromtxt(f, skip_header=1, delimiter=','))
+        classes = apply_resource('data_sets', "diabetes_classification.csv",
+                                 lambda f: np.genfromtxt(f, delimiter=','))
+
+        model = MclustDA(data, classes)
+        print(model.loglik())
+        print(model.df())
+
+        model = EDDA(data, classes)
+        print(model.loglik())
+        print(model.df())
+
+
+
+
 
 
